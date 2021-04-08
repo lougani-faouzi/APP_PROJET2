73b7c9d7d5392126d88e1b0c2abcf31b82ab3d45
lougani-faouzi <faouzi.lougani@hotmail.com>
2021-04-08 23:59:06 +0200

    Parallele calcul de la somme MPI

--- mpi.c
+++ mpi.c
@@ -12,13 +12,46 @@ int size;
   Le processus 0 doit obtenir le résultat dans la variable somme
 */
 #define max_iter_exo1 1000
+#define ROOT_RANK 0
 void exo1(){
-  int i; 
-  int somme = 0;
 
-  for(i = 0; i < max_iter_exo1; i++){
-    somme += i; 
-  }
+  int plage_debut,plage_fin;
+
+   
+  int i; 
+  int somme=0,s=0;
+  
+  // recuperer le nombre de processus total
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  // recuperer le rang 
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  // si le rang recuperé n'est pas egale au rang du processus 0 
+  if(rank!=ROOT_RANK)  
+  {
+       // on calcule la plage d'iteration 
+  	plage_debut=rank*max_iter_exo1/size;
+  	plage_fin=(rank*max_iter_exo1/size)+max_iter_exo1/size; 
+       
+       //on calcule la somme pour le processus du rang rank 
+  	for(i=plage_debut;i<plage_fin;i++)
+  	{
+		s=s+i;
+        }
+        MPI_Reduce(&s, &somme, 1, MPI_INT, MPI_SUM, ROOT_RANK, MPI_COMM_WORLD);
+  
+  }else // si le rang recuperé est egale au rang du processus 0 
+  {
+        // on fixe la plage d'iteration pour le processus root (processus 0)
+        plage_debut=0;
+        plage_fin=max_iter_exo1/size;
+        //on calcule la somme pour le processus du rang 0 
+        for(i=plage_debut;i<plage_fin;i++){
+		s=s+i;
+        }
+        MPI_Reduce(&s, &somme, 1, MPI_INT, MPI_SUM, ROOT_RANK, MPI_COMM_WORLD);
+  
   
   /*Vérification du résultat*/
   fprintf(stderr,"Somme %d attendu %d\n",somme, ((max_iter_exo1-1)*max_iter_exo1)/2);
@@ -26,6 +59,8 @@ void exo1(){
   assert(somme == ((max_iter_exo1-1)*max_iter_exo1)/2);
   /*Vérification du nombre d'iération réalisées par chaque processus MPI*/
   assert(i == max_iter_exo1/size);
+  
+  }
 }
 
 
