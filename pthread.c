a2dc58d37d8201c0212e1bab770999de112d9987
lougani-faouzi <faouzi.lougani@hotmail.com>
2021-04-13 21:31:39 +0200

    Convertir le code du fichier openmp.c en phtreads

--- pthread.c
+++ pthread.c
@@ -1,20 +1,89 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 /*
 Convertir le code du fichier openmp.c exo1 en code utilisant uniquement les fonctions pthread
 */
+
+#define NB_THREADS_EXO1 4
 #define max_iter_exo1 1000
-void exo1(){
-  int i;
-  int somme = 0;
-  for(i = 0; i < max_iter_exo1; i++){
-    somme += i; 
-  }
-  /*Vérification du résultat*/
-  fprintf(stderr,"Somme %d attendu %d\n",somme, ((max_iter_exo1-1)*max_iter_exo1)/2);
-  assert(somme == ((max_iter_exo1-1)*max_iter_exo1)/2);
+
+
+typedef struct 
+{       int somme;     // corresspond a variable qui contiendra la somme 
+	int val_iter;  // correspond ala valeur de l'iteration i dans le code openmp 
+	pthread_mutex_t lock_mutex;// un verrou 
+}infos;
+
+
+void *critique(void *a)
+{
+   infos *f=NULL;
+   int i;
+   
+   //on récupére l'identifiant du thread crée par pthread_create (notre cas la fonction exo1())
+   pthread_t thread_id = pthread_self();
+
+   //verouiller le mutex apres avoir fait un cast de *a vers le type infos  
+   f=(infos*)a;
+   pthread_mutex_lock(&(*f).lock_mutex);
+   
+   // calculer de la somme  
+   for(i=(*f).val_iter;i<max_iter_exo1;i++)
+   {   
+	(*f).somme=(*f).somme+(*f).val_iter;
+	(*f).val_iter=(*f).val_iter+1;
+   }
+
+   // libérer le mutex 
+   pthread_mutex_unlock(&(*f).lock_mutex);
+   pthread_exit(NULL);
+   
+}
+
+
+void  exo1(){
+
+
+    int i=0,j=0; 
+    infos f;
+    
+    // allocation du tableau qui va contenir les threads
+    pthread_t *threads=NULL;
+    threads = malloc(NB_THREADS_EXO1*sizeof(pthread_t));
+    
+    // Initialisation du type 
+    f.val_iter = 0;
+    f.somme = 0;
+    pthread_mutex_init(&f.lock_mutex,NULL);
+
+    //création des threads
+     while(i<NB_THREADS_EXO1)
+     {
+        //chaque thread execute la fonction critique() en prenant comme argument la structure f
+	pthread_create(&*(threads+i),NULL,critique,&f);
+	i++;
+     }
+    
+     //fin d'execution des threads
+     while(i<NB_THREADS_EXO1)
+     {
+       pthread_join(*(threads+i),NULL);
+       j++;
+     }
+    
+      // Destruction du mutex
+      pthread_mutex_destroy(&f.lock_mutex);
+   
+      //desalocation du tableau threads
+      free(threads);
+
+
+ /*Vérification du résultat*/
+  fprintf(stderr,"Somme %d attendu %d\n",f.somme, ((max_iter_exo1-1)*max_iter_exo1)/2);
+  assert(f.somme == ((max_iter_exo1-1)*max_iter_exo1)/2);
 }
 
 /*
@@ -56,7 +125,7 @@ int main(int argc, char** argv){
   fprintf(stderr,"PThread\n");
   
   exo1();
-  exo2();
+  //exo2();
   
   return 0; 
 }
